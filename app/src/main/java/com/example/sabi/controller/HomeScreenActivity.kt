4a35ffac898dd9ab6db49ac0a5b8740d86792ed3
rp.wang <deng_feng520@163.com>
2026-07-16 23:07:50 +0800

    Modify home screen view

--- app/src/main/java/com/example/sabi/controller/HomeScreenActivity.kt
+++ app/src/main/java/com/example/sabi/controller/HomeScreenActivity.kt
@@ -1,7 +1,27 @@
 package com.example.sabi.controller
 
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.sabi.ui.theme.SaBiTheme
+import com.example.sabi.view.HomeScreenView
 
-
-class HomeScreenActivity {
-
+class HomeScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SaBiTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreenView()
+                }
+            }
+        }
+    }
 }
